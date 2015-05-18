9e2f1d95f964d0a464d78567e8978ec3f7a55318
zethra <jediben97@gmail.com>
2015-05-18 14:17:50 -0400

    Added libs and index page

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
+<?php include 'header.html' ?>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+        <div class="container">  
+            <p class="h1">Welcome to the Hacker's Puzzle</p>
+            <p class="h4">To complete the puzzle simple press enter open the vault and enter to correct password</p>
+        </div>
+<?php include 'footer.html' ?>
+<!--cGFzc3dvcmQ9 44 69 64 20 79 6f 75 20 72 65 61 6c 6c 79 20 74 68 69 6e 6b 20 69 74 20 77 6f 75 6c 64 20 62 65 20 74 68 61 74 20 65 61 73 79 3f PWRyb3dzc2Fw-->
